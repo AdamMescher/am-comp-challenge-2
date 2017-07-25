a1f45bdab9e030b33d93224cf55fcfd7cb6c02a2
Adam Mescher <admescher@gmail.com>
2017-07-25 13:07:07 -0600

    submit

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log('connected')
+//
+// $('.header-nav-shares').mouseover(function() {
+//   $('.icon-shares').prop('src', 'images/icon-share-white.svg');
+// });
+//
+// $('.header-nav-shares').mouseleave(function() {
+//   $('.icon-shares').prop('src', 'images/icon-share-blue.svg');
+// })
+//
+// $('.header-nav-playlists').on('mouseover', function(){
+//   $('.icon-playlists').prop('src', 'images/icon-playlists-white.svg');
+// })
+//
+// $('.header-nav-personal-videos').on('mouseover', function() {
+//   $('.icon-playlists').prop('src', 'images/icon-playlists-blue.svg');
+// })
+//
+// $('.admin-icon-triangle').on('mouseover', function() {
+//
+// })
